54df811bd445798b16fe8ad5a92b39bcf6f6781f
@artwalkkonzept <artwalkkonzept@gmail.com>
2021-09-24 13:03:19 +0200

    first push

--- server/record_db.js
+++ server/record_db.js
@@ -50,17 +50,31 @@ class Db {
      * @param count The amount of artwalks to add.
      * @returns {Promise} Resolves when everything has been saved.
      */
-    async bootstrap(count = 1) {
+    async bootstrap(count = 4) {
         const article_description = ['ild 1', 'ild 2', 'ild 3', 'ild 4','ild 5', 'ild 6', 'ild 7', 'ild 8', 'ild 9', 'ild 10'];
-        const  article_title = ['ZKM-Tour', 'Landschaften', 'Imprissionistische Landschaften', 'Tour'];
-        
+        function getRandomInt(min, max) {
+            return Math.floor(Math.random() * (4) + min);
+        }
+
+        function getRandomArticle_title() {
+            return ['ZKM-Tour', 'Landschaften', 'Imprissionistische Landschaften', 'Tour'][getRandomInt(0,3)]
+        }
+
+        function getRandomArticle_description() {
+            const shuffled = article_description.sort(() => 0.5 - Math.random());
+            return shuffled.slice(0, getRandomInt(1,shuffled.length));
+        }
+
+        let l = (await this.getRecords()).length;
+        console.log("Record collection size:", l);
+
         if (l === 0) {
             let promises = [];
 
             for (let i = 0; i < count; i++) {
                 let record = new this.recordModel({
-                    article_title: getArticle_title(),
-                    article_description: getArticle_description()
+                    article_title: getRandomArticle_title(),
+                    article_description: getRandomArticle_description()
                 });
                 promises.push(record.save());
             }
